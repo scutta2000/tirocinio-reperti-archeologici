5b6b2ac1ed858a330af9ebca1edc2f0c9edb0de0
scutta2000 <scuttari.pietro@gmail.com>
2021-04-01 14:41:12 +0200

    Database non classificato

--- src/scripts/get_data.py
+++ src/scripts/get_data.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 def ceramiche_DB():
     path = Path(__file__).parent.parent / 'dati_ceramiche_classi.xlsx'
-    print(path)
     tarquina = pd.read_excel(path, index_col=0, usecols=[
                              0, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
@@ -15,3 +14,10 @@ def ceramiche_DB():
     y = [1]*len(tarquina) + [0]*len(non_tarquina)
 
     return X, y
+
+
+def da_classificare_DB():
+    path = Path(__file__).parent.parent / 'dati_ceramiche_da_classificare.xlsx'
+    db = pd.read_excel(path, index_col=0, usecols=[
+        0, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+    return db
